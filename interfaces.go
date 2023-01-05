40ef5318faa350be825b5246b112743b363b24b2
Daniel Pineda <pijesusdaniel@gmail.com>
2023-01-05 15:30:12 -0600

    Cambia geometria public a private

--- interfaces.go
+++ interfaces.go
@@ -2,12 +2,12 @@ package figuras
 
 import "fmt"
 
-type Geometria interface {
+type geometria interface {
 	area() float32
 	perimetro() float32
 }
 
-func Medidas(g Geometria) {
+func Medidas(g geometria) {
 	fmt.Println("Medidas: ", g)
 	fmt.Println("Area: ", g.area())
 	fmt.Println("Perimetro: ", g.perimetro())
